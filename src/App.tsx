306f448dae186ede28905ea1123faf0f655f4be9
Aayush Karki <akarki@ramapo.edu>
2023-03-21 00:42:33 -0400

    adds a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import GameGrid from "./components/GameGrid";
+import GameHeading from "./components/GameHeading";
 import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/PlatformSelector";
@@ -48,22 +49,25 @@ function App() {
 				</GridItem>
 			</Show>
 			<GridItem area="main">
-				<Flex paddingLeft={2} marginBottom={5}>
-					<Box marginRight={5}>
-						<PlatformSelector
-							onSelectedPlatform={(platform) =>
-								setGameQuery({ ...gameQuery, platform })
+				<Box paddingLeft={2}>
+					<GameHeading gameQuery={gameQuery} />
+					<Flex marginBottom={5}>
+						<Box marginRight={5}>
+							<PlatformSelector
+								onSelectedPlatform={(platform) =>
+									setGameQuery({ ...gameQuery, platform })
+								}
+								selectedPlatform={gameQuery.platform}
+							/>
+						</Box>
+						<SortSelector
+							onSelectSortOrder={(sortOrder) =>
+								setGameQuery({ ...gameQuery, sortOrder })
 							}
-							selectedPlatform={gameQuery.platform}
+							sortOrder={gameQuery.sortOrder}
 						/>
-					</Box>
-					<SortSelector
-						onSelectSortOrder={(sortOrder) =>
-							setGameQuery({ ...gameQuery, sortOrder })
-						}
-						sortOrder={gameQuery.sortOrder}
-					/>
-				</Flex>
+					</Flex>
+				</Box>
 
 				<GameGrid gameQuery={gameQuery} />
 			</GridItem>
---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,20 @@
+import { Heading } from "@chakra-ui/react";
+import { GameQuery } from "../App";
+
+interface GameHeadingProps {
+	gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: GameHeadingProps) => {
+	const heading = `${gameQuery.platform?.name || ""} ${
+		gameQuery.genre?.name || ""
+	} Games`;
+
+	return (
+		<Heading as="h1" marginY={5} fontSize="5xl">
+			{heading}
+		</Heading>
+	);
+};
+
+export default GameHeading;
