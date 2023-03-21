2b241e0ecee62c196eed6e948c7488a8db6a3411
Aayush Karki <akarki@ramapo.edu>
2023-03-20 23:57:59 -0400

    BUGFIX: fixs issue with chakra menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -42,20 +42,22 @@ function App() {
 				</GridItem>
 			</Show>
 			<GridItem area="main">
-				<HStack paddingLeft={2} marginBottom={5} spacing={5}>
-					<PlatformSelector
-						onSelectedPlatform={(platform) =>
-							setGameQuery({ ...gameQuery, platform })
-						}
-						selectedPlatform={gameQuery.platform}
-					/>
+				<Flex paddingLeft={2} marginBottom={5}>
+					<Box marginRight={5}>
+						<PlatformSelector
+							onSelectedPlatform={(platform) =>
+								setGameQuery({ ...gameQuery, platform })
+							}
+							selectedPlatform={gameQuery.platform}
+						/>
+					</Box>
 					<SortSelector
 						onSelectSortOrder={(sortOrder) =>
 							setGameQuery({ ...gameQuery, sortOrder })
 						}
 						sortOrder={gameQuery.sortOrder}
 					/>
-				</HStack>
+				</Flex>
 
 				<GameGrid gameQuery={gameQuery} />
 			</GridItem>
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,11 +1,4 @@
-import {
-	Box,
-	Button,
-	Menu,
-	MenuButton,
-	MenuItem,
-	MenuList,
-} from "@chakra-ui/react";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
@@ -23,27 +16,21 @@ const PlatformSelector = ({
 	if (error) return null;
 
 	return (
-		<Box>
-			<Menu>
-				<MenuButton
-					as={Button}
-					rightIcon={<BsChevronDown />}
-					margin={1}
-				>
-					{selectedPlatform?.name || "Platforms"}
-				</MenuButton>
-				<MenuList>
-					{data.map((platform) => (
-						<MenuItem
-							onClick={() => onSelectedPlatform(platform)}
-							key={platform.id}
-						>
-							{platform.name}
-						</MenuItem>
-					))}
-				</MenuList>
-			</Menu>
-		</Box>
+		<Menu>
+			<MenuButton as={Button} rightIcon={<BsChevronDown />} margin={1}>
+				{selectedPlatform?.name || "Platforms"}
+			</MenuButton>
+			<MenuList>
+				{data.map((platform) => (
+					<MenuItem
+						onClick={() => onSelectedPlatform(platform)}
+						key={platform.id}
+					>
+						{platform.name}
+					</MenuItem>
+				))}
+			</MenuList>
+		</Menu>
 	);
 };
 
